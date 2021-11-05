e0355a88c877f97f3a16f8c8ff9f63c75d8ce0b2
ilakosterin <ilya@in10.top>
2021-11-05 16:25:27 +0300

    Добавил мета тэги сео: title & description

--- pages/contact.vue
+++ pages/contact.vue
@@ -6,7 +6,17 @@
 import ContactPage from '~/components/ContactPage'
 export default {
   name: 'contact',
-  components: { ContactPage }
+  components: { ContactPage },
+  head: {
+    title: 'Неокартины художника Алексея Анисимова | Контакты',
+    meta: [
+      {
+        hid: 'description',
+        name: 'desсription',
+        content: 'Контактная информация художника Алексея Анисимова. Понравились работы? Пишите, я обязательно отвечу.'
+      }
+    ]
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,17 @@
 <script>
 import MainPage from '~/components/MainPage'
 export default {
-  components: { MainPage }
+  components: { MainPage },
+  head: {
+    title: 'Неокартины художника Алексея Анисимова | Главная',
+    meta: [
+      {
+        hid: 'description',
+        name: 'desсription',
+        content: 'Неокартины - уникальный интеллектуально-художественный продукт, синтезирующий изобразительное искусство, дизайн и поэзию.'
+      }
+    ]
+  }
 }
 </script>
 <style scoped>
